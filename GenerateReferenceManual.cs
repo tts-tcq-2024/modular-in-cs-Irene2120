58470569585dd1c4527c6ddca1c197dfc1f4c67a
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 13:15:05 +0530

    Update GenerateReferenceManual.cs

--- GenerateReferenceManual.cs
+++ GenerateReferenceManual.cs
@@ -7,19 +7,23 @@ namespace TelCo.ColorCoder
 {
   partial class GenerateRefrenceManual
   {
-    public Color MajorColor , MinorColor;
+    public Color[] MajorColor;
+    public Color[] MinorColor;
+    
     /// <summary>
     /// Print the mapping between color-names to the corresponding numbers.
     /// </summary>
     private static string ReferenceManualGenerator()
     {
+      GenerateRefrenceManual.MinorColor;
+      GenerateRefrenceManual.MajorColor;
       int colorCode = 1;
       string manual = "_________________________________________\n";
       manual += String.Format("|{0}\t|{1}\t|{2}|\n", "Major Color", "Minor Color", "Pair No");
 
-      foreach(var majorColor in colorMapMajor)
+      foreach(var majorColor in MajorColor)
       {
-        foreach(var minorColor in colorMapMinor)
+        foreach(var minorColor in MinorColor)
         {
           manual += String.Format("|{0}\t\t|{1}\t\t|{2}|\n", MajorColor.Name, MinorColor.Name, colorCode);
           colorCode++;
